68187f5b2a0820d7779299f0e65b906641a2a505
emipaz <65197702+emipaz@users.noreply.github.com>
2020-12-17 20:47:46 -0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,35 @@
+from django.http import JsonResponse
+from django.views import View
+from django.shortcuts import render
+
+# Create your views here.
+
+def json(modeladmin, request, queryset):
+    json = []
+    for q in queryset:
+        json.append({
+            'pregunta': q.pregunta,
+            'respuestas': list(q.respuestas.all().values_list('respuesta', flat=True))
+        })
+    return JsonResponse(json, safe=False)
+
+json.short_description = "Exportar selección a JSON"
+
+class PruebaView(View):
+    template_name="templates/prueba.html"
+
+    def get(self,request,*args,**kwargs):
+
+        p=request
+        context={"p":"hola"}
+        return  render(p,self.template_name,context)
+
+    def post (self,request,*args,**kwargs):
+        import json
+        p = request
+        #print(p.__dict__.keys(),sep="\n")
+        #print(p.read().decode())
+        res = dict(p.POST)
+        print(res)
+        context = {"p": "Chau"}
+        return render(p, self.template_name, context)
